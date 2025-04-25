a4f86db699e88d23d21aa4237cf0a4362085166f
vamsi2702 <vamsi.srivathsa@iitgn.ac.in>
2025-04-25 17:52:25 +0530

    lab11

---
+++ AlarmConsoleApp/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace AlarmConsoleApp
+{
+    // Publisher class
+    public class AlarmClock
+    {
+        // Define the delegate and event
+        public delegate void AlarmEventHandler(object sender, EventArgs e);
+        public event AlarmEventHandler RaiseAlarm;
+
+        private readonly TimeSpan targetTime;
+
+        public AlarmClock(TimeSpan targetTime)
+        {
+            this.targetTime = targetTime;
+        }
+
+        // Method to check time
+        public void CheckTime()
+        {
+            Console.WriteLine("Alarm set. Monitoring time...");
+
+            while (true)
+            {
+                TimeSpan currentTime = DateTime.Now.TimeOfDay;
+
+                if (currentTime.Hours == targetTime.Hours &&
+                    currentTime.Minutes == targetTime.Minutes &&
+                    currentTime.Seconds == targetTime.Seconds)
+                {
+                    // Raise the event when the target time is reached
+                    OnRaiseAlarm(EventArgs.Empty);
+                    break;
+                }
+
+                // Wait for a second before checking again
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+
+        // Method to raise the event
+        protected virtual void OnRaiseAlarm(EventArgs e)
+        {
+            RaiseAlarm?.Invoke(this, e);
+        }
+    }
+
+    // Subscriber class
+    public class AlarmListener
+    {
+        public void Ring_Alarm(object sender, EventArgs e)
+        {
+            Console.WriteLine("ALARM! The target time has been reached!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan targetTime;
+            bool validInput = false;
+
+            // Get input from user and validate
+            do
+            {
+                Console.Write("Enter alarm time (HH:MM:SS): ");
+                string timeInput = Console.ReadLine();
+
+                if (TimeSpan.TryParse(timeInput, out targetTime))
+                {
+                    validInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid time format! Please use HH:MM:SS format.");
+                }
+            } while (!validInput);
+
+            // Create publisher object
+            AlarmClock alarmClock = new AlarmClock(targetTime);
+
+            // Create subscriber object
+            AlarmListener listener = new AlarmListener();
+
+            // Subscribe to the event
+            alarmClock.RaiseAlarm += listener.Ring_Alarm;
+
+            // Start checking the time
+            alarmClock.CheckTime();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AlarmWinFormsApp/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AlarmWinFormsApp  // Changed from AlarmApp to match Designer.cs
+{
+    // Event arguments for alarm event
+    public class AlarmEventArgs : EventArgs
+    {
+        public DateTime AlarmTime { get; }
+
+        public AlarmEventArgs(DateTime alarmTime)
+        {
+            AlarmTime = alarmTime;
+        }
+    }
+
+    // Publisher class
+    public class AlarmClock
+    {
+        // Define delegate and event
+        public delegate void AlarmEventHandler(object sender, AlarmEventArgs e);
+        public event AlarmEventHandler RaiseAlarm;
+
+        // Fully qualified Timer to avoid ambiguity
+        private System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+        private TimeSpan targetTime;
+
+        public AlarmClock()
+        {
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+        }
+
+        public void SetAlarm(TimeSpan time)
+        {
+            targetTime = time;
+            timer.Start();
+        }
+
+        public void StopAlarm()
+        {
+            timer.Stop();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan currentTime = DateTime.Now.TimeOfDay;
+
+            if (currentTime.Hours == targetTime.Hours &&
+                currentTime.Minutes == targetTime.Minutes &&
+                currentTime.Seconds == targetTime.Seconds)
+            {
+                timer.Stop();
+                OnRaiseAlarm(new AlarmEventArgs(DateTime.Now));
+            }
+        }
+
+        // Method to raise the event
+        protected virtual void OnRaiseAlarm(AlarmEventArgs e)
+        {
+            RaiseAlarm?.Invoke(this, e);
+        }
+
+        // Method to expose timer for subscribers
+        public event EventHandler TimerTick
+        {
+            add { timer.Tick += value; }
+            remove { timer.Tick -= value; }
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        private AlarmClock alarmClock = new AlarmClock();
+        private Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupUI();
+
+            // Subscribe to events
+            alarmClock.RaiseAlarm += Ring_Alarm;
+            alarmClock.TimerTick += ChangeBackgroundColor;
+        }
+
+        private void SetupUI()
+        {
+            // Create UI components if they don't exist yet
+            if (Controls.Count == 0)
+            {
+                this.Text = "Alarm Application";
+                this.Size = new Size(400, 200);
+
+                Label lblTime = new Label
+                {
+                    Text = "Enter Time (HH:MM:SS):",
+                    Location = new Point(20, 20),
+                    AutoSize = true
+                };
+                Controls.Add(lblTime);
+
+                TextBox txtTime = new TextBox
+                {
+                    Name = "txtTime",
+                    Location = new Point(20, 50),
+                    Size = new Size(150, 25)
+                };
+                Controls.Add(txtTime);
+
+                Button btnStart = new Button
+                {
+                    Name = "btnStart",
+                    Text = "Start Alarm",
+                    Location = new Point(20, 90),
+                    Size = new Size(100, 30)
+                };
+                btnStart.Click += btnStart_Click;
+                Controls.Add(btnStart);
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            TextBox txtTime = Controls.Find("txtTime", true)[0] as TextBox;
+
+            if (TimeSpan.TryParse(txtTime.Text, out TimeSpan targetTime))
+            {
+                alarmClock.SetAlarm(targetTime);
+            }
+            else
+            {
+                MessageBox.Show("Invalid time format! Please use HH:MM:SS.");
+            }
+        }
+
+        private void ChangeBackgroundColor(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256));
+        }
+
+        private void Ring_Alarm(object sender, AlarmEventArgs e)
+        {
+            MessageBox.Show($" Alarm Time Reached at {e.AlarmTime.ToLongTimeString()}!");
+        }
+
+        // Add this method to handle the Form1_Load event referenced in Designer.cs
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Any initialization code for when the form loads
+        }
+    }
+}
